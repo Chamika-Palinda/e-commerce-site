af810eaf7e26dbc6e310960a88b414658d375691
OfficialChamika <chamikarathnayake890@gmail.com>
2021-08-07 20:46:42 +0530

    dispatch the signout action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,20 @@ import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector,useDispatch } from 'react-redux';
 import SigninScreen from './screens/SigninScreen';
+import { signout } from './actions/userActions';
 
 function App() {
   const cart = useSelector(state => state.cart);
   const { cartItems } = cart;
-
+  const userSignin = useSelector(state => state.userSignin);
+  const { userInfo } = userSignin; 
+  const dispatch = useDispatch();
+  
+  const signoutHandler = () =>{
+    dispatch(signout());
+  }
 
   return (
     <BrowserRouter>
@@ -27,7 +34,22 @@ function App() {
             { cartItems.length > 0 && ( <span className="badge">{cartItems.length}</span>
             )}
             </Link>
-            <Link to="/signin">Sign In</Link>
+            {
+              userInfo ?(
+                <div className="dropdown">
+
+                <Link to ="#">{userInfo.name}<i className="fa fa-caret-down"></i>{' '}
+                </Link>
+                <ul className="dropdown-content">
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                </ul>
+                </div>
+              ): (
+                <Link to="/signin">Sign In</Link>
+              ) 
+            }
           </div>
         </header>
         <main>
